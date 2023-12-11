b2f06be0d5a62d400611e58320d7b9dd59d90566
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2023-12-11 19:27:16 +0300

    added a dirty script loop -- wip

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@ export hostname="debiandeclarative"
 export username="debianuser"
 export apt_packages="vim neofetch zsh fish cowsay htop"
 
+# the apt_packages thing will be parsed by awk kek
+
 export project_directory="./" # (dirty hack) TODO: replace it
 
 export chroot_directory="$1" # do not access $1 ever again
 
 
-./make.sh "$@"
+if [ -z ${jank} ]; then
+	./make.sh "$@"
+fi
--- main.sh
+++ main.sh
@@ -2,12 +2,19 @@
 
 # do not edit this script except for fixing issues with the basic setup
 
+# dirty, but helps to parse args fast
+
+jank="yes" # kek
+${project_directory}/build.sh "$@" 
+
 echo "installing in chroot..." # debug
 apt-get update
 apt-get upgrade -y
 apt-get install -y systemd bash # debug, currently wip
 ### installing user-defined packages (build.sh)
-#apt-get install -y ${apt_packages} # invoked in make.sh #TODO: make awk parser of packages
+sleep 10 # debug 
+"echo sneeding...." # debug
+apt-get install -y ${apt_packages} 
 
 # invoke other functions
 # there should be a for cycle with files in function directory :) #TODO
