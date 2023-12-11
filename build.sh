d20343e755d5cbda56028d6ca376c8b5b8dcb5ad
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2023-12-11 18:04:55 +0300

    wip

--- build.sh
+++ build.sh
@@ -11,14 +11,5 @@ export project_directory="./" # (dirty hack) TODO: replace it
 
 export chroot_directory="$1" # do not access $1 ever again
 
-echo "run as root"
 
-if [ -z "$1" ]; then
-	echo "provide a chroot directory"
-	exit 1
-fi
-
-printf "%i\n" 1
-printf "%s\n" "printing out main.sh" 
-
-./main.sh "$@"
+./make.sh "$@"
