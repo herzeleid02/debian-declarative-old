16077c4d23a5ed34e44ad8727283355a3e31d6a4
herzeleid02 <inxbox360.timosha@gmail.com>
2023-12-11 21:59:08 +0300

    fixed the issue with importing

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 echo "installing in chroot..." # debug
 apt-get update
 apt-get upgrade -y
-apt-get install -y systemd bash # debug, currently wip
+apt-get install -y systemd bash less
 ### installing user-defined packages (build.sh)
 apt-get install -y ${apt_packages}
 
