7a3cf87674ae7d1c9a294bee876378cb4a02554b
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2023-12-11 18:20:09 +0300

    it works!!!

--- main.sh
+++ main.sh
@@ -3,5 +3,7 @@
 # do not edit this script except for fixing issues with the basic setup
 
 echo "installing in chroot..." # debug
+apt-get update
+apt-get upgrade -y
 apt-get install -y systemd bash neofetch vim neovim htop cowsay fish # debug, currently wip
 echo "done installing in chroot..." # debug
