e4b6d24dedc1e4fd7574e77739c301ea4a7a9b8b
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2023-12-11 13:37:11 +0300

    commit before i redo the process (check README)

--- build.sh
+++ build.sh
@@ -2,17 +2,21 @@
 # no posix :(
 
 # 
-export image="debian:12"
+export container_image="debian:12"
 export hostname="debiandeclarative"
 export username="debianuser"
 export apt_packages="vim neofetch zsh"
 
-export chroot_directory="$2" # do not access $2 ever again
+export chroot_directory="$1" # do not access $1 ever again
 
-if [ -z "$2" ]; then
+echo "run as root"
+
+if [ -z "$1" ]; then
 	echo "provide a chroot directory"
 	#exit 1
 fi
 
+printf "%i\n" 1
+printf "%s\n" "printing out main.sh" 
 
 ./main.sh "$@"
